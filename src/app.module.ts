aaeeb9edbd058cea78619161e483bba2f1beb19e
bertdev <herbertmatos10@hotmail.com>
2022-05-29 17:39:55 -0300

    configuracoes basicas para primeiro resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
